ea1c5027fbddf7252f76debc6f1ac92306f41097
rohit <rohitmahto062@gmail.com>
2026-03-21 18:21:01 +0530

    feat: Enhance hero section with carousel functionality and style adjustments

--- mini-projects/E-Commerse/static/script/script.js
+++ mini-projects/E-Commerse/static/script/script.js
@@ -2,7 +2,7 @@ let slideIndex = 0;
 
 let carousel = () => {
   let x = document.getElementsByClassName("hero-img-slide");
-  console.log(x);
+  const dots = document.querySelectorAll(".dot");
 
   for (let i = 0; i < x.length; i++) {
     x[i].style.display = "none";
@@ -10,6 +10,10 @@ let carousel = () => {
   slideIndex++;
   if (slideIndex > x.length) slideIndex = 1;
   x[slideIndex - 1].style.display = "block";
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  dots[slideIndex - 1].classList.add("active");
   setTimeout(carousel, 5000);
 };
 
