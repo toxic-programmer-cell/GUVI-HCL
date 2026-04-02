2f26d795950cf758afd535b25443ac9440849d16
rohit <rohitmahto062@gmail.com>
2026-04-02 19:10:29 +0530

    js data types

---
+++ javascript/dataTypeInJs.js
@@ -0,0 +1,36 @@
+console.log("js-4 Data types");
+
+// Primitive data type
+// Number
+let x = 10;
+let y = 10.5;
+console.log(x, typeof x);
+console.log(y, typeof y);
+
+// String
+let str = "Hello";
+console.log(str, typeof str);
+
+// Boolean (true/false)
+let isBool = true;
+let isBool1 = false;
+console.log(isBool, typeof isBool);
+console.log(isBool1, typeof isBool1);
+
+// Undefine
+let undef;
+console.log(undef, typeof undef); //undefine
+
+// Null
+let nul = null;
+console.log(nul, typeof nul); //object
+
+// Symbol
+let symbol = Symbol("hello");
+console.log(symbol, typeof symbol); //symbol
+
+// BigInt (2 power 53-1)2^53-1
+let bigInt = 123456789736367472635477583645253647346537473645n;
+console.log(bigInt, typeof bigInt); //bigint
+
+//
