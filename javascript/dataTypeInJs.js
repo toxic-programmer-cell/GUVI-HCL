91c0a0fd9509a9ab0cba4b2ce2f19eb999c2b75a
rohit <rohitmahto062@gmail.com>
2026-04-03 15:55:48 +0530

    feat: reorganize and expand data type examples in JavaScript

--- javascript/dataTypeInJs.js
+++ javascript/dataTypeInJs.js
@@ -33,4 +33,45 @@ console.log(symbol, typeof symbol); //symbol
 let bigInt = 123456789736367472635477583645253647346537473645n;
 console.log(bigInt, typeof bigInt); //bigint
 
-//Array
+//Object
+let obj = { name: "Rohit", age: 23 };
+console.log(obj, typeof obj); //object
+
+// Arrays
+let arr = [1, 2, 3, 4, 5];
+console.log(arr, typeof arr); //object
+
+//Functions
+function fn(arg) {
+  console.log("Hello", arg);
+}
+fn("Rohit");
+console.log(fn, typeof fn); //function
+
+// Date
+let date = new Date();
+console.log(date, typeof date); //object
+
+// RefExp
+let re = /ark-guvi+xyz/;
+console.log(re, typeof re); //object
+
+// Map
+let map = new Map();
+map.set("name", "Rohit");
+map.set("age", 23);
+console.log(map, typeof map); //object
+
+// Set
+let set = new Set([1, 2, 3]);
+console.log(set, typeof set); //object
+
+//Special values
+let max = Infinity;
+let min = -Infinity;
+console.log(max, typeof max); //number
+console.log(min, typeof min); //number
+
+// NaN
+let notANumber = NaN;
+console.log(notANumber, typeof notANumber); //number
