5422b65e8d09465f130bd1adf8027db447f1ddfe
rohit <rohitmahto062@gmail.com>
2026-04-10 14:38:19 +0530

    fix: correct variable names and improve todo rendering logic

--- mini-projects/Todo-list/script.js
+++ mini-projects/Todo-list/script.js
@@ -1,17 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const taskInput = document.getElementById("taskInput");
+  const todoInput = document.getElementById("taskInput");
   const addTodoBtn = document.getElementById("addTodoBtn");
   const todoList = document.getElementById("todoList");
 
   const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-  function addTodo() {
-    const taskText = taskInput.value.trim();
-    if (taskInput === "") return;
-    todos.push({ text: taskText, completed: false });
-    taskInput.value = "";
+  function renderTodo() {
+    todoList.innerHTML = "";
+    todos.forEach((todo, index) => {
+      const taskList = document.createElement("li");
+      taskList.className =
+        "list-items-group d-flex justify-content-between align-items-center";
+
+      const todoSpan = document.createElement("span");
+      todoSpan.textContent = todo.text;
+
+      const btnDiv = document.createElement("div");
+      btnDiv.className = "d-flex gap-2";
+
+      const completeBtn = document.createElement("button");
+      completeBtn.className = "btn btn-success btn-sm";
+      completeBtn.innerText = "Complete";
+      completeBtn.addEventListener("click", () => {
+        completeTodo(index);
+      });
+
+      if (todo.completed) {
+        todoSpan.classList.add("completed");
+      }
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className = "btn btn-danger btn-sm";
+      deleteBtn.innerText = "Delete";
+      deleteBtn.addEventListener("click", () => {
+        deleteTodo(index);
+      });
+
+      todoList.appendChild(taskList);
+
+      taskList.appendChild(todoSpan);
+      taskList.appendChild(btnDiv);
+
+      btnDiv.appendChild(completeBtn);
+      btnDiv.appendChild(deleteBtn);
+    });
+  }
+
+  todoInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  });
+
+  function deleteTodo(index) {
+    todos.splice(index, 1);
     saveTodo();
-    renderTodos();
+    renderTodo();
+  }
+
+  function completeTodo(index) {
+    todos[index].completed = !todos[index].completed;
+    saveTodo();
+    renderTodo();
+  }
+
+  function addTodo() {
+    const todoText = todoInput.value.trim();
+
+    if (todoText === "") return;
+
+    todos.push({ text: todoText, completed: false });
+    todoInput.value = "";
+    saveTodo();
+    renderTodo();
   }
 
   function saveTodo() {
@@ -20,60 +81,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
   addTodoBtn.addEventListener("click", addTodo);
 
-  taskInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      addTodo();
-    }
-  });
-
-  function renderTodos() {
-    todoList.innerHTML = "";
-    todos.forEach((todos, index) => {
-      const listItem = document.createElement("li");
-      listItem.className =
-        "list-group-item d-flex justify-content-between aling-items-center ";
-
-      listItem.textContent = todos.text;
-
-      if (todos.completed) {
-        listItem.classList.add("completed");
-      }
-
-      const completeBtn = document.createElement("button");
-      completeBtn.className = "btn btn-success btn-sm";
-      completeBtn.textContent = "complete";
-      completeBtn.addEventListener("click", () => {
-        toggleCompleteTask(index);
-      });
-
-      const deleteBtn = document.createElement("button");
-      deleteBtn.className = "btn btn-danger btn-sm";
-      deleteBtn.textContent = "Delete";
-      deleteBtn.addEventListener("click", () => {
-        deleteTask(index);
-      });
-
-      const btnDiv = document.createElement("div");
-      btnDiv.className = "d-flex gap-2";
-      btnDiv.appendChild(completeBtn);
-      btnDiv.appendChild(deleteBtn);
-
-      listItem.appendChild(btnDiv);
-      todoList.appendChild(listItem);
-    });
-  }
-
-  function toggleCompleteTask(index) {
-    todos[index].completed = !todos[index].completed;
-    saveTodo();
-    renderTodos();
-  }
-
-  function deleteTask(index) {
-    todos.splice(index, 1);
-    saveTodo();
-    renderTodos();
-  }
-
-  renderTodos();
+  renderTodo();
 });
