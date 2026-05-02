7d0791a2b29064ed1a7aed903a3b04586eb3e483
rohit <rohitmahto062@gmail.com>
2026-05-02 19:08:27 +0530

    feat: initialize React application with essential files, components, and configurations

---
+++ react/projects/first-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FilterableProductTable from './components/FilterableProductTable'
+
+const App = () => {
+  return (
+    <>
+    <FilterableProductTable />
+    </>
+  )
+}
+
+export default App
